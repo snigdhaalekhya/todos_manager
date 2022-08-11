caab02d5e197941d2795292e4e4e213e3f7acf5e
snigdha_alekhya <pvn.snigdhaalekhya@gmail.com>
2022-08-11 11:01:48 +0530

    changes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,8 @@ class TodosController < ApplicationController
     def index
         render plain: Todo.all.map{|todo| todo.to_pleasent_string}.join("\n")
     end
+    def show
+        id=params[:id]
+        todo=Todo.find(id)
+        render plain: todo.to_pleasent_string
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "todos", to: "todos#index"
+  get "todos/:id", to: "todos#show"
 end
