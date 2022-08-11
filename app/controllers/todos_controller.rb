83190d400bc1de469e4b10c442531dc73ebce519
snigdha_alekhya <pvn.snigdhaalekhya@gmail.com>
2022-08-11 10:50:48 +0530

    change

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
     def index
-        render plain: Todo.all.map{|todo| todo.to_pleasent_string}
+        render plain: Todo.all.map{|todo| todo.to_pleasent_string}.join("\n")
     end
 end 
